cfd7b07de7a9431edea2fb026c78c4b3fda73a3f
Hossam Othman <rowheadrex@gmail.com>
2023-09-18 12:44:52 +0200

    nav function added

---
+++ 2nd_galarie_dcode/main.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.carousel').forEach(carousel => {
+    const items = carousel.querySelectorAll('.carousel__item');
+    const buttonsHtml = Array.from(items, () => {
+        return `<span class="carousel__button"></span>`;
+    });
+
+    carousel.insertAdjacentHTML("beforeend", `
+    <div class="carousel__nav">
+        ${buttonsHtml.join("")}
+    </div>
+    `);
+    
+    const buttons = carousel.querySelectorAll('.carousel__button');
+    buttons.forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            // unselecet all the items
+            items.forEach(item => item.classList.remove("carousel__item--selected"));
+            buttons.forEach(xbtn => xbtn.classList.remove("carousel__button--selected"));
+
+            items[i].classList.add('carousel__item--selected');
+            btn.classList.add('carousel__button--selected');
+        })
+    });
+});
+
